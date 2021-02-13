5c721205ccb4b6deeeb6407452eaf02dfb9f0c06
José Eduardo <jhernandez@compuevolucion.com>
2021-02-12 18:37:47 -0600

    video 9 de 10

--- Js/ejercicios.js
+++ Js/ejercicios.js
@@ -570,11 +570,83 @@ console.clear();
 //25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
 //********  EJERCICIO 25********/
 const duplicated = (arr = []) => {
-  console.log(arr.sort());
+  let uniqueValues = [...new Set(arr)];
+  console.log(uniqueValues);
 };
 duplicated(["x", 10, "x", 2, "10", 10, true, true]);
+console.clear();
 //********  EJERCICIO 25 FIN********/
 
 //26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
 //********  EJERCICIO 26********/
+const avg = (arr = []) => {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = num + arr[i];
+  }
+  let resultAvg = num / arr.length;
+  console.log(resultAvg);
+};
+avg([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
+console.clear();
 //********  EJERCICIO 26 FIN********/
+
+/* 27) Programa una clase llamada Pelicula.
+
+La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
+  - Todos los datos del objeto son obligatorios.
+  - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
+     7 restantes números.
+  - Valida que el título no rebase los 100 caracteres.
+  - Valida que el director no rebase los 50 caracteres.
+  - Valida que el año de estreno sea un número entero de 4 dígitos.
+  - Valida que el país o paises sea introducidos en forma de arreglo.
+  - Valida que los géneros sean introducidos en forma de arreglo.
+  - Valida que los géneros introducidos esten dentro de los géneros 
+     aceptados*.
+  - Crea un método estático que devuelva los géneros aceptados*.
+  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
+    decimal de una posición.
+  - Crea un método que devuelva toda la ficha técnica de la película.
+  - Apartir de un arreglo con la información de 3 películas genera 3 
+    instancias de la clase de forma automatizada e imprime la ficha técnica 
+    de cada película.
+
+* Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
+ */
+
+//********  EJERCICIO 27********/
+class Peliculas {
+  constructor(
+    IdPeliculaIMDB,
+    Titulo,
+    Director,
+    AñoDeEstreno,
+    PaisDeOrigen,
+    Genero,
+    CalificacionIMDB
+  ) {
+    this.IdPeliculaIMDB = IdPeliculaIMDB;
+    this.Titulo = Titulo;
+    this.Director = Director;
+    this.AñoDeEstreno = AñoDeEstreno;
+    this.PaisDeOrigen = PaisDeOrigen;
+    this.Genero = Genero;
+    this.CalificacionIMDB = CalificacionIMDB;
+  }
+  pelicula() {
+    if (
+      this.IdPeliculaIMDB == undefined ||
+      this.Titulo == undefined ||
+      this.Director == undefined ||
+      this.AñoDeEstreno == undefined ||
+      this.PaisDeOrigen == undefined ||
+      this.Genero == undefined ||
+      this.CalificacionIMDB == undefined
+    ) {
+      return console.warn("Favor de ingresar todos los valores");
+    }
+  }
+}
+
+//********  EJERCICIO 27 FIN********/
