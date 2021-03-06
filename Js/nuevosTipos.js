9fbf2c0d8eb4f32471bb7895f591eeefc3a31940
José Eduardo <jhernandez@compuevolucion.com>
2021-03-06 00:07:59 -0600

    Set

--- Js/nuevosTipos.js
+++ Js/nuevosTipos.js
@@ -32,3 +32,48 @@ for (let propiedad in PERSONA) {
 }
 
 console.log(Object.getOwnPropertySymbols(PERSONA));
+console.clear();
+
+//note: El tipo de dato 'set' es similar al arreglo sin embargo que se compone de datos únicos
+const set = new Set([1, 2, 3, 3, 5, true, false, {}, "Hola"]);
+
+console.log(set);
+console.log(set.size);
+
+const set2 = new Set([]);
+
+set2.add(1);
+set2.add(1);
+set2.add(2);
+set2.add(3);
+set2.add("Adios");
+set2.add([]);
+set2.add({});
+set2.add(true);
+set2.add(true);
+
+console.log(set2);
+console.log(set2.size);
+
+console.log("Set2");
+for (const iterator of set2) {
+  console.log(iterator);
+}
+
+console.log("Set como arreglo");
+let arr = Array.from(set2);
+console.log(arr[0]);
+
+console.log("Eliminacion de valores del set");
+
+set2.delete("Adios");
+console.log(set2);
+
+console.log(
+  "Comprueba si existe un valor. Regresa true si existe | false si no"
+);
+console.log(set2.has(3));
+
+console.log("Para limpiar un set");
+set2.clear();
+console.log(set2);
