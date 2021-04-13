ccf0889c9844f163875ce89201e907401fae0ff2
José Eduardo <jhernandez@compuevolucion.com>
2021-04-12 23:59:39 -0500

    QuerySelector

--- Js/dom.js
+++ Js/dom.js
@@ -1,4 +1,4 @@
-console.log("******Elementos del documento*******");
+/* console.log("******Elementos del documento*******");
 console.log(window.document);
 console.log(document);
 console.log(document.head);
@@ -12,4 +12,23 @@ console.log(document.images);
 console.log(document.forms);
 setTimeout(() => {
   console.log(document.getSelection().toString());
-}, 2000);
+}, 2000); */
+
+//? https://developer.mozilla.org/es/docs/Web/API/Node/nodeType
+
+console.log(document.getElementsByTagName("li"));
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+console.log(document.getElementById("menu"));
+//?Los elementos anteriores han sido reemplazados por estos métodos
+console.log(document.querySelector("#menu"));
+console.log(document.querySelector("a"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach((el) => {
+  console.log(el);
+});
+console.log(document.querySelector(".card"));
+console.log(document.querySelectorAll(".card"));
+console.log(document.querySelectorAll(".card")[2]);
+console.log(document.querySelectorAll("#menu li"));
